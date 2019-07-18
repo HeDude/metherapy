3b8222a916434989ac0e6568271eacd5dc5fae1b
bijbrengen <slagter@bijbrengen.nl>
2019-07-18 14:21:40 +0200

    Started new version of website by test.php

---
+++ test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8"/>
+    <title>MeTherapy - Pyschosociale & Hypnotherapie bij autisme, afvallen en onverklaarbare lichamelijke klachten.</title>
+
+<?php echo file_get_contents( "https://www.hedude.com/php/favicon.php" ); ?>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body id="webpage" class="metherapy">
+    <section id="hedude_popup_message">
+        <img class="popup_message" src="image/popup_message.png">
+    </section>
+    <script src="https://www.hedude.com/scripts/popup_message.js"></script>
+</body>
+</html>
