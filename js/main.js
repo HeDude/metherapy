138bbda4cadc5fc076e425c599bd67cfa14158ea
bijbrengen <slagter@bijbrengen.nl>
2019-07-28 09:32:26 +0200

    New info and pictures in scaling hart on diffirent devices

--- js/main.js
+++ js/main.js
@@ -2,24 +2,15 @@ $( document ).ready
 (
 	function()
 	{
-		var fade_time_unit = 300;
+		var fade_time_unit = 200;
 		var fade_time      = 4 * fade_time_unit;
 		var i;
 		var j;
 
-		$( "#menu" ).fadeOut( 0, "swing" );
 		$( "#webpage" ).css( "visibility", "visible");
 		$( "#home" ).fadeIn( fade_time, "swing" );
 
-		$( "#popup_message" ).click
-		(
-			function ( event )
-			{
-				$( "#popup_message" ).css( "display", "none");
-			}
-		)
-
-		for (i = 1;i < 6; i++)
+		for (i = 0;i < 6; i++)
 		{
 			$("p#metherapy_menu_text0"+i).fadeOut( 0, "swing");
 			$("img#metherapy_foto0"+i ).fadeOut( 0, "swing" );
@@ -29,26 +20,37 @@ $( document ).ready
 				function ( event )
 				{
 					$( this ).finish();
-					for (j = 1; j < 6; j++)
+					for (j = 0; j < 6; j++)
 					{
 						if ( j !== event.data.active_button )
 						{
-							$( "p#metherapy_menu_text0" + j ).fadeOut( fade_time, "swing" );
-							$( "img#metherapy_foto0" + j ).fadeOut( fade_time, "swing" );
-							$( "img#Letter_W_MeTherapy_0" + j ).attr( 'src', "images/Letter_W_MeTherapy.svg" );
+							$( "p#metherapy_menu_text0" + j ).fadeOut( 0, "swing" );
+							$( "img#metherapy_foto0" + j ).fadeOut( 0, "swing" );
 							$( "a#bee_button_0" + j ).css( { "cursor": "pointer" } );
 						};
 					};
-					$( "p#metherapy_menu_text0" + event.data.active_button ).fadeIn( fade_time, "swing" );
-					$( "img#metherapy_foto0" + event.data.active_button ).fadeIn( fade_time, "swing" );
-					$( "img#Letter_W_MeTherapy_0" + event.data.active_button ).attr( 'src', "images/Letter_W_MeTherapy_Selected.svg" );
+					$( "p#metherapy_menu_text0" + event.data.active_button ).fadeIn( 0, "swing" );
+					$( "img#metherapy_foto0" + event.data.active_button ).fadeIn( 0, "swing" );
 					$( "a#bee_button_0" + event.data.active_button ).css( { "cursor": "not-allowed" } );
 				}
 			);
 		}
-		$( "p#metherapy_menu_text01" ).fadeIn( fade_time, "swing" );
-		$( "img#metherapy_foto01" ).fadeIn( fade_time, "swing" );
-		$( "img#Letter_W_MeTherapy_01" ).attr( 'src', "images/Letter_W_MeTherapy_Selected.svg" );
-		$( "a#bee_button_01" ).css( { "cursor": "not-allowed" } );
+		$( "p#metherapy_menu_text00" ).fadeIn( 0, "swing" );
+		$( "img#metherapy_foto00" ).fadeIn( 0, "swing" );
+		$( "a#bee_button_00" ).css( { "cursor": "not-allowed" } );
+	}
+);
+
+
+/* Scaling function */
+var $el = $("#very-specific-design");
+var elWidth = $el.outerWidth();
+var $wrapper = $("#scaleable-wrapper");
+var scale;
+scale = $wrapper.width() / elWidth;
+$el.css
+(
+	{
+		transform: "translate(-50%, -50%) " + "scale(" + scale + ")"
 	}
 );
--- new.php
+++ new.php
@@ -2,58 +2,51 @@
 <html lang="nl">
 <head>
     <meta charset="utf-8"/>
-    <title>MeTherapy - Pyschosociale & Hypnotherapie bij onverklaarbare lichamelijke klachten, afvallen en autisme.</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MeTherapy - Mindfulness & Hypnotherapie bij lichamelijke klachten, afvallen en autisme.</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 </head>
 <body id="webpage" class="metherapy">
     <section id="home">
-        <h1>Menu</h1>
-        <img id="home_button" class="metherapy_home_icon" src="images/baseline-home-24px.svg" alt="Home Icon">
+        <div class="scaleable-wrapper" id="scaleable-wrapper">
+            <div class="very-specific-design" id="very-specific-design">
+                <h1>Menu</h1>
 
-        <p class="metherapy_afspraak_tekst">Afspraak</p>
-        <p id="metherapy_menu_text01" class="metherapy_menutext">MeTherapy staat voor <b>T</b>ijd en <b>A</b>andacht voor jezelf, om door <b>A</b>fstand te nemen van jezelf, <b>B</b>ewustwording en <b>I</b>nzicht te krijgen. Door bewust en onbewust te focussen, komen je hart en hoofd meer in balans, en kom je <b>D</b>ichter bij <b>J</b>ezelf.<br/><br/><i>Lees meer op <a href="http://wiki.metherapy.nl" target="_blank">Me Wiki.</a></i></p>
-        <p id="metherapy_menu_text02" class="metherapy_menutext">MeTherapy helpt bij angst, onzekerheid, faalangst, stress, burn-out, pijn, onverklaarbare lichamelijke klachten, PDS, depressie, somberheid, verslavingen en tal van andere problemen waarbij de samenwerking tussen lichaam en geest heel belangrijk is.<br/><br/><i>Lees meer op <a href="http://wiki.metherapy.nl" target="_blank">Me Wiki.</a></i></p>
-        <p id="metherapy_menu_text03" class="metherapy_menutext">Vaak weet je al (jaren lang) veel over je kwaal of klacht, maar ondanks van alles er aan gedaan te hebben, lost het probleem niet op. Integrale psycho- en hypnotherapie maakt gebruik van onbewuste processen in jezelf, waardoor het kwartje nu eindelijk kan vallen en klachten verminderen of verdwijnen.<br/><br/><i>Lees meer op <a href="http://wiki.metherapy.nl" target="_blank">Me Wiki.</a></i></p>
-        <p id="metherapy_menu_text04" class="metherapy_menutext">MeTherapy wordt gerund door Mari&euml;lle Janssen: maatschappelijk ondernemer, bewegings-wetenschapper en integraal psycho- en hypnotherapeut. Mari&euml;lle is als therapeute aangesloten bij de <a href="https://hypnotherapie.nl/praktijk-pagina/me-therapy-me-coaching/" target="_blank">NBVH</a> en <a href="https://samensterkzorg.nl/de-hulpverleners/mari-lle" target="_blank">SSZ</a>, waardoor vergoeding mogelijk is vanuit de aanvullende verzekering.<br/><br/><i>Lees meer op <a href="http://wiki.metherapy.nl" target="_blank">Me Wiki.</a></i></p>
-        <p id="metherapy_menu_text05" class="metherapy_menutext">Zowel overdag als s'avonds kan er een afspraak gemaakt worden. De behandeling is aan de Geuzingerbrink 94 te Emmen en kan eventueel online worden toegepast.<br/><br/><i>Lees meer op <a href="http://wiki.metherapy.nl" target="_blank">Me Wiki.</a></i></p>
+                <p id="metherapy_menu_text00" class="metherapy_menutext">MeTherapy wordt gerund door <a href="https://www.linkedin.com/in/marielle-janssen-b62b402b/" target="_blank">Mari&euml;lle Janssen</a>: maatschappelijk ondernemer, bewegingswetenschapper en integraal psycho- en hypnotherapeut. Mari&euml;lle is aangesloten bij de <a href="https://hypnotherapie.nl/praktijk-pagina/me-therapy-me-coaching/" target="_blank">NBVH</a>, <a href="https://samensterkzorg.nl/de-hulpverleners/mari-lle" target="_blank">SSZ</a>, <a href="https://www.scag.nl/register/?q=Me+Therapy+%26+Me+Coaching" target="_blank">SCAG</a> en <a href="https://www.rbcz.nu" target="_blank">RBCZ</a> waardoor vergoeding mogelijk is vanuit de aanvullende verzekering of jeugdhulp. De behandeling kan overdag of s'avonds zowel aan huis, aan de Geuzingerbrink 94 te Emmen als online.<br/><br/><i>Lees meer op <a href="http://wiki.metherapy.nl" target="_blank">Me Wiki.</a></i></p>
+                <p id="metherapy_menu_text01" class="metherapy_menutext">Mindfulness helpt je bewust te worden van wat er allemaal in je lichaam en geest gebeurt zonder er nog in te worden gezogen. Deze aanpak is effectief bij autisme, verslavingen, depressie en lichamelijke klachten.<br/><br/><i>Lees meer op <a href="http://wiki.metherapy.nl" target="_blank">Me Wiki.</a></i></p>
+                <p id="metherapy_menu_text02" class="metherapy_menutext">Hypnotherapie zorgt dat je via hypnose contact krijgt met onbewuste processen. De trance is licht zodat je actief betrokken blijft bij deze verkenning. Deze aanpak is effectief bij het prikkelbaar darmsyndroom, afvallen en psychische klachten die zich lichamelijk uiten.<br/><br/><i>Lees meer op <a href="http://wiki.metherapy.nl" target="_blank">Me Wiki.</a></i></p>
+                <p id="metherapy_menu_text03" class="metherapy_menutext">Vaak weet je al (jaren lang) veel over je kwaal of klacht, maar ondanks van alles er aan gedaan te hebben, lost het probleem niet op. Integrale psycho- en hypnotherapie maakt gebruik van onbewuste processen in jezelf, waardoor het kwartje nu eindelijk kan vallen en klachten verminderen of verdwijnen.<br/><br/><i>Lees meer op <a href="http://wiki.metherapy.nl" target="_blank">Me Wiki.</a></i></p>
+                <p id="metherapy_menu_text04" class="metherapy_menutext">MeTherapy wordt gerund door Mari&euml;lle Janssen: maatschappelijk ondernemer, bewegings-wetenschapper en integraal psycho- en hypnotherapeut. Mari&euml;lle is als therapeute aangesloten bij de <a href="https://hypnotherapie.nl/praktijk-pagina/me-therapy-me-coaching/" target="_blank">NBVH</a> en <a href="https://samensterkzorg.nl/de-hulpverleners/mari-lle" target="_blank">SSZ</a>, waardoor vergoeding mogelijk is vanuit de aanvullende verzekering.<br/><br/><i>Lees meer op <a href="http://wiki.metherapy.nl" target="_blank">Me Wiki.</a></i></p>
+                <p id="metherapy_menu_text05" class="metherapy_menutext">Zowel overdag als s'avonds kan er een afspraak gemaakt worden. De behandeling is aan de Geuzingerbrink 94 te Emmen en kan eventueel online worden toegepast.<br/><br/><i>Lees meer op <a href="http://wiki.metherapy.nl" target="_blank">Me Wiki.</a></i></p>
 
-        <img class="metherapy_menu_left_Border" src="images/Menu_Button_00_MeTherapy.svg" alt="Menu MeTherapy">
-        <img id="metherapy_menu_left_01" class="metherapy_menu_left" src="images/Menu_Button_01_MeTherapy.svg" alt="Menu Button 01 MeTherapy">
-        <img id="metherapy_menu_left_02" class="metherapy_menu_left" src="images/Menu_Button_02_MeTherapy.svg" alt="Menu Button 02 MeTherapy">
-        <img id="metherapy_menu_left_03" class="metherapy_menu_left" src="images/Menu_Button_03_MeTherapy.svg" alt="Menu Button 03 MeTherapy">
-        <img id="metherapy_menu_left_04" class="metherapy_menu_left" src="images/Menu_Button_04_MeTherapy.svg" alt="Menu Button 04 MeTherapy">
-        <img id="metherapy_menu_left_05" class="metherapy_menu_left" src="images/Menu_Button_05_MeTherapy.svg" alt="Menu Button 05 MeTherapy">
+                <img class="metherapy_menu_left_Border" src="images/Menu_Button_00_MeTherapy.svg" alt="Menu MeTherapy">
+                <img id="metherapy_menu_left_00" class="metherapy_home_icon" src="images/baseline-home-24px.svg" alt="Home Icon">
+                <img id="metherapy_menu_left_01" class="metherapy_menu_left" src="images/Menu_Button_01_MeTherapy.svg" alt="Menu Button 01 MeTherapy">
+                <img id="metherapy_menu_left_02" class="metherapy_menu_left" src="images/Menu_Button_02_MeTherapy.svg" alt="Menu Button 02 MeTherapy">
+                <img id="metherapy_menu_left_03" class="metherapy_menu_left" src="images/Menu_Button_03_MeTherapy.svg" alt="Menu Button 03 MeTherapy">
+                <img id="metherapy_menu_left_04" class="metherapy_menu_left" src="images/Menu_Button_04_MeTherapy.svg" alt="Menu Button 04 MeTherapy">
+                <img id="metherapy_menu_left_05" class="metherapy_menu_left" src="images/Menu_Button_05_MeTherapy.svg" alt="Menu Button 05 MeTherapy">
 
-        <a id="bee_button_01" class="metherapy_button MeTherapy_1" href="#Waarom">Psychosociale therapie</a>
-        <a id="bee_button_02" class="metherapy_button MeTherapy_2" href="#Wanneer">Hypnotherapie</a>
-        <a id="bee_button_03" class="metherapy_button MeTherapy_3" href="#Wat">Lichamelijke<br/>Klachten</a>
-        <a id="bee_button_04" class="metherapy_button MeTherapy_4" href="#Wie">Afvallen</a>
-        <a id="bee_button_05" class="metherapy_button MeTherapy_5" href="#Waar">Autisme</a>
+                <a id="bee_button_00" class="metherapy_button MeTherapy_0" href="#home">Afspraak</a>
+                <a id="bee_button_01" class="metherapy_button MeTherapy_1" href="#home">Mindfulness</a>
+                <a id="bee_button_02" class="metherapy_button MeTherapy_2" href="#home">Hypnotherapie</a>
+                <a id="bee_button_03" class="metherapy_button MeTherapy_3" href="#home">Lichamelijke<br/>Klachten</a>
+                <a id="bee_button_04" class="metherapy_button MeTherapy_4" href="#home">Afvallen</a>
+                <a id="bee_button_05" class="metherapy_button MeTherapy_5" href="#home">Autisme</a>
 
-        <img id="metherapy_foto01" class="metherapy_foto" src="image/psychosociaal.png" alt="Foto Meisje">
-        <img id="metherapy_foto02" class="metherapy_foto" src="images/wanneer.jpg" alt="Foto Klachten">
-        <img id="metherapy_foto03" class="metherapy_foto" src="images/wat.jpg" alt="Foto">
-        <img id="metherapy_foto04" class="metherapy_foto" src="images/pasfoto.jpg" alt="Pasfoto">
-        <img id="metherapy_foto05" class="metherapy_foto" src="images/waar.png" alt="Foto">
-    </section>
-    <section id="common">
-        <h1>Common</h1>
-        <img class="metherapy_title" src="images/Logo_MeTherapy_Alleen_Naam.svg" alt="Logo MeTherapy">
-        <img id="logo" class="metherapy_logo" src="images/logo.svg" alt="Logo MeTherapy">
-        <a class="metherapy_therapist" href="https://www.linkedin.com/in/marielle-janssen-b62b402b/" target="_blank"><img class="metherapy_therapist" src="images/therapist.jpg" alt="Therapeut MeTherapy"></a>
-        <a class="metherapy_maps" href="https://goo.gl/maps/AbjCtwbrptp" target="_blank"><img class="metherapy_rand" src="images/maps.png" alt="Maps"></a>
-        <img class="metherapy_contact" src="images/Visitekaartje.png" alt="Visitekaartje MeTherapy">
-        <p class="metherapy_copyright">&copy; 2016-2019 MeTherapy</p>
-        <aside>
-            <a href="https://samensterkzorg.nl/de-hulpverleners/mari-lle" target="_blank"><img class="metherapy_ssz metherapy_rand" src="images/logo-SSZ.png" alt="Samen Sterk Zorg"></a>
-            <a href="https://www.scag.nl/register/?q=Me+Therapy+%26+Me+Coaching" target="_blank"><img class="metherapy_scag metherapy_rand" src="images/logo-scag.png" alt="SCAG"></a>
-            <a href="https://www.rbcz.nu" target="_blank"><img class="metherapy_rbcz metherapy_rand" src="images/Logo-RBCZ.png" alt="RBCZ"></a>
-            <a href="https://hypnotherapie.nl/praktijk-pagina/me-therapy-me-coaching/" target="_blank"><img class="metherapy_nbvh metherapy_rand" src="images/logo-nbvh.png" alt="NBVH"></a>
-        </aside>
-        <blockquote class="metherapy_compensation" cite="https://wiki.metherapy.nl/index.php/Praktijk#Vergoeding">
-            <a href="https://wiki.metherapy.nl/index.php/Praktijk#Vergoeding" target="_blank">Vergoeding</a> mogelijk.
-        </blockquote>
+                <img id="metherapy_foto00" class="metherapy_foto" src="image/afspraak.png" alt="Foto Meisje">
+                <img id="metherapy_foto01" class="metherapy_foto" src="image/psychosociaal.png" alt="Foto Meisje">
+                <img id="metherapy_foto02" class="metherapy_foto" src="images/wanneer.jpg" alt="Foto Klachten">
+                <img id="metherapy_foto03" class="metherapy_foto" src="images/wat.jpg" alt="Foto">
+                <img id="metherapy_foto04" class="metherapy_foto" src="images/pasfoto.jpg" alt="Pasfoto">
+                <img id="metherapy_foto05" class="metherapy_foto" src="images/waar.png" alt="Foto">
+
+                <img class="metherapy_title_me" src="image/logo_naam_me.svg" alt="Logo MeTherapy">
+                <img class="metherapy_title_therapy" src="image/logo_naam_therapy.svg" alt="Logo MeTherapy">
+            </div>
+        </div>
     </section>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
